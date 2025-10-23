13775c739bebe4fb7383560cb05d682446f15849
lucas-costello <153712867+lucas-costello@users.noreply.github.com>
2025-10-23 01:23:32 +0000

    website_setup

--- index.js
+++
@@ -1,9 +0,0 @@
-//  Javascript
-function updatemenu() {
-  if (document.getElementById('responsive-menu').checked == true) {
-    document.getElementById('menu').style.borderBottomRightRadius = '0';
-    document.getElementById('menu').style.borderBottomLeftRadius = '0';
-  }else{
-    document.getElementById('menu').style.borderRadius = '50px';
-  }
-}
